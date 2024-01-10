e63cdcfd883635cc346670286b5b305c3e31c357
Andrey-66 <andrey66-66-66@yandex.ru>
2024-01-10 14:23:32 +0300

    Add custom block class, fix light

--- src/main/java/com/dimas_studio/tfbr/block/ModBlocks.java
+++ src/main/java/com/dimas_studio/tfbr/block/ModBlocks.java
@@ -20,7 +20,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
     public static final RegistryObject<Block> RESPAWN_NAGA = registryBlock("respawn_naga",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.BEDROCK)));
+            RespawnBlock::new);
 
 
     private static <T extends Block>RegistryObject<T> registryBlock(String name, Supplier<T> block) {
