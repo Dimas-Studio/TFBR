6fe9ca5b959d6cd7eea8bea9a153ca5b51a2d66f
Andrey-66 <andrey66-66-66@yandex.ru>
2024-01-13 12:41:46 +0300

    Done

--- src/main/java/com/dimas_studio/tfbr/TFBR.java
+++ src/main/java/com/dimas_studio/tfbr/TFBR.java
@@ -6,7 +6,9 @@ import com.dimas_studio.tfbr.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.TickEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
@@ -53,16 +55,15 @@ public class TFBR
         ModBlocks.register(modEventBus);
         ModItemGroup.register(modEventBus);
 
-        // Register ourselves for server and other game events we are interested in
         NeoForge.EVENT_BUS.register(this);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
         // Do something when the server starts
         LOGGER.info("HELLO from server starting");
+        LOGGER.info(Config.HYDRA_TROPHY_BLOCK.get());
     }
 }
